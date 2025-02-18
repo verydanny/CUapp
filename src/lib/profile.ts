4f0322384004bafda09f43592ef1478988d7d245
Daniil Veremchuk <daniel@verydanny.com>
2025-02-18 04:18:12 -0500

    cleanup

--- src/lib/profile.ts
+++ src/lib/profile.ts
@@ -1,42 +1,11 @@
-import { type Models, Query } from 'node-appwrite'
+import { Query } from 'node-appwrite'
 import { type RequestEvent } from '@sveltejs/kit'
 import { createAdminClient } from './server/auth/appwrite'
-import { getProfileImageUrls, getSingleProfileImageUrl } from './utils/imageUtils'
+import { getSingleProfileImageUrl } from './utils/imageUtils'
 import { getSingleDocumentByQuery } from './server/databaseHelpers'
 import { ADMIN_LABEL, IS_PRIVATE_PROFILE } from './const'
 
-const { databases, storage } = createAdminClient()
-
-const normalizeProfileImageUrls = (profileImageFiles: Models.FileList) => {
-    const profileImageUrls = getProfileImageUrls(profileImageFiles?.files)
-
-    return [
-        profileImageUrls.highest,
-        profileImageUrls.high,
-        profileImageUrls.medium,
-        profileImageUrls.low
-    ].filter(Boolean)
-}
-
-export async function normalizeProfileData(profile?: Models.Document) {
-    if (!profile?.$id) {
-        return {
-            profile: null,
-            profileImageUrlsArray: null
-        }
-    }
-
-    const profileImages = await storage.listFiles('profile-images', [], profile?.$id)
-
-    return {
-        profile: {
-            username: profile?.username as string,
-            bio: profile?.bio as string,
-            permissions: profile?.permissions as string[]
-        },
-        profileImageUrlsArray: normalizeProfileImageUrls(profileImages)
-    }
-}
+const { databases } = createAdminClient()
 
 export async function fetchProfileFromLocals({ locals, cookies }: RequestEvent) {
     const { user } = locals
